49ab9f3c413933ca2226ed89d97ae9fa90d6fa01
Atul Jadhav <jadhavatul222@gmail.com>
2021-01-06 22:14:52 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Container, View, Left, Right, Button, Icon, Item, Input , Text , Colors , color } from 'native-base';
+
+
+// Our custom files and classes import
+//import Colors from '../Colors';
+//import Text from '../component/Text';
+//import Navbar from '../component/Navbar';
+
+export default class Login extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        username: '',
+        password: '',
+        hasError: false,
+        errorText: ''
+      };
+  }
+
+
+  render() {
+    var left = (
+      <Left style={{flex:1}}>
+        <Button onPress={() => Actions.pop()} transparent>
+          <Icon name='ios-arrow-back' />
+        </Button>
+      </Left>
+    );
+    var right = (
+      <Right style={{flex:1}}>
+        <Button onPress={() => Actions.search()} transparent>
+          <Icon name='ios-search-outline' />
+        </Button>
+        <Button onPress={() => Actions.cart()} transparent>
+          <Icon name='ios-cart' />
+        </Button>
+      </Right>
+    );
+    return(
+      <Container style={{backgroundColor: '#fdfdfd'}}>
+        {/* <Navbar left={left} right={right} title="LOGIN" /> */}
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', paddingLeft: 50, paddingRight: 50}}>
+          <View style={{marginBottom: 35, width: '100%'}}>
+            <Text style={{fontSize: 24, fontWeight: 'bold', textAlign: 'left', width: '100%', color: '#ff0000'}}>Welcome back, </Text>
+            <Text style={{fontSize: 18, textAlign: 'left', width: '100%', color: '#687373'}}>Login to continue </Text>
+          </View>
+          <Item>
+              <Icon active name='ios-person' style={{color: "#687373"}}  />
+              <Input placeholder='Username' onChangeText={(text) => this.setState({username: text})} placeholderTextColor="#687373" />
+          </Item>
+          <Item>
+              <Icon active name='ios-lock' style={{color: "#687373"}} />
+              <Input placeholder='Password' onChangeText={(text) => this.setState({password: text})} secureTextEntry={true} placeholderTextColor="#687373" />
+          </Item>
+          {this.state.hasError?<Text style={{color: "#c0392b", textAlign: 'center', marginTop: 10}}>{this.state.errorText}</Text>:null}
+          <View style={{alignItems: 'center'}}>
+            <Button onPress={() => this.login()} style={{backgroundColor: '#ff0000', marginTop: 20}}>
+              <Text style={{color: '#fdfdfd'}}>Login</Text>
+            </Button>
+          </View>
+        </View>
+      </Container>
+    );
+  }
+}
